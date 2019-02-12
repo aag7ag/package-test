9a5ec48bcd1bdea1018766bb4f4e1d83b9644215
arturo alvarez <luis.alvarez@indatcom.mx>
2019-02-12 10:46:29 -0600

    first commit

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dd('hello world!!');
+    }
+}
